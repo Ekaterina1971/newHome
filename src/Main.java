f5dae8402b42908d6808edfbd0afcce65b128257
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-07-31 16:46:37 +0300

    изменен вывод

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет этому миру!");
+        System.out.println("Привет этому дивному миру!");
     }
 }
